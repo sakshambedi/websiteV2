2c7370b61c16d26e43a7efc4c140e06383c62b7f
Saksham Bedi <bedis3@myumanitoba.ca>
2024-09-04 20:58:46 -0500

    added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 
 import type { Metadata } from "next";
-import { Inter, Roboto_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { cn } from "@/lib/utils"
 import SiteConfig from "@/config/site";
 import { ThemeProvider } from "@/components/theme-provider"
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+import { Analytics } from '@vercel/analytics/react';
 
 
 const robotoMono = Roboto_Mono({
@@ -28,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <body className={cn("w-screen max-w-fullfont-sans antialiased flex flex-row justify-center phone:-min-w-screen phone:overflow-x-hidden md:overflow-x-hidden", robotoMono.variable)}>
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
